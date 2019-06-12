cea8f65081011bba64d78708d387367526a8e6c2
Vince <so_vince@outlook.com>
2019-06-12 08:54:42 +0800

    update

--- src/main/scala/com/sovince/batch/transformation/DataSetTransformationTest.scala
+++ src/main/scala/com/sovince/batch/transformation/DataSetTransformationTest.scala
@@ -1,5 +1,6 @@
 package com.sovince.batch.transformation
 
+import com.sovince.util.DbFakeUtil
 import org.apache.flink.api.scala._
 
 /**
@@ -18,8 +19,19 @@ object DataSetTransformationTest {
     data.map(_*10).filter(_>30).print()
   }
 
+  def mapPartition(): Unit ={
+    val data = env.fromCollection(List(1,2,3,4,5,6,7,8,9,10))
+    data.mapPartition(x=>{
+      val con = DbFakeUtil.getConnection
+      println(con)
+      println("???")
+      //x是一个集合collection
+      x
+    }).print()
+  }
+
   def main(args: Array[String]): Unit = {
-    mapAndFilter()
+    mapPartition()
   }
 
 }
